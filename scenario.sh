744cc3da2dd84863f6f78a87eb6b3e5ae835e5a1
Yann <yann@fery.me>
2017-06-27 16:09:45 +0200

    Version 0.2

--- scenario.sh
+++ scenario.sh
@@ -3,4 +3,5 @@
 
 rm -f /tmp/coin8.sqlite3
 RUST_BACKTRACE=1 target/debug/rusty --database /tmp/coin8.sqlite3 subscribe http://www.geekzone.fr/feed/podcast/dans-le-canap
+RUST_BACKTRACE=1 target/debug/rusty --database /tmp/coin8.sqlite3 subscribe https://feeds.soundcloud.com/users/soundcloud:users:139721529/sounds.rss
 RUST_BACKTRACE=1 target/debug/rusty --database /tmp/coin8.sqlite3 update
